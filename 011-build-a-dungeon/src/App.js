c274b96766047c3abe0573a4928cdaa5e2227695
darms <darmcca@gmail.com>
2019-06-04 21:22:36 -0700

    adding in player

--- 011-build-a-dungeon/src/App.js
+++ 011-build-a-dungeon/src/App.js
@@ -1,10 +1,13 @@
-import React, {Component } from 'react';
+import React, {Component } from 'react'
+import Player from './features/player'
 
 
 class App extends Component {
   render () {
     return (
-<div>  Awesome Game </div>
+       <div>  
+         <Player /> 
+         </div>
       
     )
 }
